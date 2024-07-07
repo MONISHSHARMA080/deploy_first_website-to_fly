dd29d53e5efe19fcfeda3f1b70aed94e7c8466bf
monish <monishsharma080@gmail.com>
2024-07-07 14:50:08 +0530

    hopefully well and good

--- both/vite.config.ts
+++ both/vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
 	plugins: [sveltekit()]
 	,server: {
 		host: '0.0.0.0', // listen on all IP addresses
+		// host: '0.0.0.0', // listen on all IP addresses
 		port: process.env.PORT || 5173, // default Vite dev server port
 		hmr: {
 		overlay: false,
